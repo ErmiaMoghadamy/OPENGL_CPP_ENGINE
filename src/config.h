c9da77cd2d79447993a730f5f5b80fd98814b9e9
Ermia <ermia.codev@gmail.com>
2026-05-30 17:51:22 +0330

    Smoke Test

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <iostream>
+
+#define WINDOW_WIDTH 1080
+#define WINDOW_HEIGHT 720
+#define WINDOW_TITLE "Balling Game"
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,14 @@
 using std::cout;
 
 int main() {
+    GLFWwindow* window;
 
+    if (!glfwInit()) { cout << "[INIT Err] Failed to initialize glfw\n"; return -1; }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
 }
